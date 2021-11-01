e0dd2f91c1fb215496a1ac98973b0a0ab0d8b174
keylperez <perez.williamcaleb@gmail.com>
2021-11-02 00:50:17 +0800

    significant changes

--- srcFolders.sh
+++ srcFolders.sh
@@ -1,51 +1,78 @@
 #project start folder script
 echo "node_modules/" >> .gitignore
+
 touch README.md
 touch index.html
+
 mkdir src
 cd src
+
 mkdir css img js scss
+
 cd css
 touch style.css
+
 cd ..
 cd js
 touch script.js
+
 cd ..
 cd scss
-mkdir abstract base layout components
+
+mkdir abstracts base layout components pages themes vendors
 touch main.scss
-cd abstract
-touch _function.scss _mixins.scss _variables.scss
+
+cd abstracts
+touch _functions.scss _mixins.scss _variables.scss _animations.scss
+
 cd .. 
+cd vendors
+# touch _bootstrap.scss jquery-ui.scss 
+
+cd ..
 cd base 
-touch _animations.scss _base.scss
+touch _reset.scss _typography.scss
+
 cd ..
 cd components
-touch _buttons.scss
-# This is a comment
+touch _buttons.scss _slider.scss
+
+cd ..
+cd pages
+touch _home.scss _about.scss _contact.scss
+
+cd ..
+cd themes
+touch _dark.scss _light.scss
+
 # Use below imports in your main main.scss file
-# @import 'abstracts/variables';
-# @import 'abstracts/mixins';
+cd ..
+echo '
+@import "abstracts/variables";
+@import "abstracts/functions";
+@import "abstracts/mixins";
+@import "abstracts/animations";
 
-# @import 'vendors/bootstrap';
+@import "vendors/bootstrap";
+@import "vendors/jquery-ui";
 
-# @import 'base/reset';
-# @import 'base/typography';
+@import "base/reset";
+@import "base/typography";
 
-# @import 'layout/navigation';
-# @import 'layout/grid';
-# @import 'layout/header';
-# @import 'layout/footer';
-# @import 'layout/sidebar';
-# @import 'layout/forms';
+@import "layout/navigation";
+@import "layout/header";
+@import "layout/footer";
+@import "layout/sidebar";
+@import "layout/forms";
 
-# @import 'components/buttons';
-# @import 'components/carousel';
-# @import 'components/cover';
-# @import 'components/dropdown';
+@import "components/buttons";
+@import "components/carousel";
+@import "components/slider";
 
-# @import 'pages/home';
-# @import 'pages/contact';
+@import "pages/home";
+@import "pages/about";
+@import "pages/contact";
 
-# @import 'themes/theme';
-# @import 'themes/admin';
+@import "themes/theme";
+@import "themes/admin";
+' >> main.scss
